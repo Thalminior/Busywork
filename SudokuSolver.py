b2be1007baaa1cc7ebc1a33cc2f6e2706996f939
unknown <exanuell@gmail.com>
2019-06-24 08:38:40 -0700

    .

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -13,26 +13,26 @@ import numpy as np
 
 # create puzzle get inputs
 
-# create lists
+# variables
 bfill = 0
 
 # test puzzle here test taken from https://en.wikipedia.org/wiki/Sudoku
-test_puzzle = np.array([[5,3,bfill,bfill,7,bfill,bfill,bfill,bfill],[6,bfill,bfill,1,9,5,bfill,bfill,bfill],[bfill,9,8,bfill,bfill,bfill,bfill,6,bfill],[8,bfill,bfill,bfill,6,bfill,bfill,bfill,3],[4,bfill,bfill,8,bfill,3,bfill,bfill,1],[7,bfill,bfill,bfill,2,bfill,bfill,bfill,6],[bfill,6,bfill,bfill,bfill,bfill,2,8,bfill],[bfill,bfill,bfill,4,1,9,bfill,bfill,5],[bfill,bfill,bfill,bfill,8,bfill,bfill,7,9]])
+test_puzzle = np.array([[5,3,4,6,7,8,9,bfill,bfill],[6,bfill,bfill,1,9,5,bfill,bfill,bfill],[bfill,9,8,bfill,bfill,bfill,bfill,6,bfill],[8,bfill,bfill,bfill,6,bfill,bfill,bfill,3],[4,bfill,bfill,8,bfill,3,bfill,bfill,1],[7,bfill,bfill,bfill,2,bfill,bfill,bfill,6],[bfill,6,bfill,bfill,bfill,bfill,2,8,bfill],[bfill,bfill,bfill,4,1,9,bfill,bfill,5],[bfill,bfill,bfill,bfill,8,bfill,bfill,7,9]])
 
-
+# blank array
 puzzle = np.array([[bfill]*9,[bfill]*9,[bfill]*9,[bfill]*9,[bfill]*9,[bfill]*9,[bfill]*9,[bfill]*9,[bfill]*9])
 
+
+# locations of blanks
 zero_locations = np.transpose(np.nonzero(test_puzzle < 1))
-# x = puzzle.put
+
+# flip on axis
 
 
-
-
+# solve row
 
 
 #solve puzzle
-
-# test across
 
 
 # print it readable
@@ -48,19 +48,22 @@ def display_array(array_in):
 
 # TODO put lines between the groups of 3
 
-# finish = 
+
 
 
 
 if __name__ == "__main__":
 
-    # print(y)
-
-
     display_array(test_puzzle)
     # display_array(y)
-    # print(y)
+    print("******")
     # display_array(finish)
 
+
+
+    
+    for i in test_puzzle[0]:
+        print(i)
+        
 
     input("push enter to close") # to keep window open
